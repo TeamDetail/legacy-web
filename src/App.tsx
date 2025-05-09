3c428724ff11d9e305448c5fda894c2b97bc2abf
dksgusdn61 <dksgusdn61@gmail.com>
2025-05-09 20:15:50 +0900

    feat : 라우터 및 기본 라이브러리 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import logo from '../public/logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./components/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-background">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>B1ND AUTH-TEMPLATE</p>
-        <a
-          className="App-link"
-          href="https://github.com/Team-B1ND/B1ND-AUTH-TEMPLATE"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          B1NDGithub
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/routes.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes as Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <>
+      <Switch>
+        <Route />
+      </Switch>
+    </>
+  );
+};
+
+export default Routes;
