da182c1f1d4361722d786129f37b175bec51320b
Cartridge0923 <pjmin0923@gmail.com>
2025-05-19 19:28:10 +0900

    Update style.ts

--- src/components/common/Card/style.ts
+++ src/components/common/Card/style.ts
@@ -4,5 +4,6 @@ import styled from "styled-components";
 export const CardContainer = styled.div<{ $size: "L" | "M" | "S" }>`
   display: flex;
   border: 3px ${LegacySementicColor.lineNeutral} solid;
-
+  border-radius: ${({ $size }) => $size === "L" ? 20 : $size === "M" ? 16 : 12};
+  
 `
