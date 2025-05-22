f9dcc2719a72e6d9f6da01908351e61b125e83e8
dksgusdn61 <dksgusdn61@gmail.com>
2025-05-22 19:36:18 +0900

    feat : store access/refresh token

--- src/hooks/Auth/useLogin.ts
+++ src/hooks/Auth/useLogin.ts
@@ -1,3 +1,4 @@
+import token from "@src/libs/token/token";
 import { TokenType } from "@src/types/Auth/TokenResponse.type";
 import axios from "axios";
 import { useRef } from "react";
@@ -6,11 +7,13 @@ import { NavigateFunction, useNavigate } from "react-router-dom";
 const useLogin = async (code: string | null, navigate: NavigateFunction) => {
   try {
     console.log(code);
-    const data = await axios.post<TokenType>(
+    const { data } = await axios.post<TokenType>(
       "https://9335-221-168-22-205.ngrok-free.app/kakao/code",
       { code: code }
     );
     if (data) {
+      token.setToken("ACCESS_TOKEN", data.accessToken);
+      token.setToken("REFRESH_TOKEN", data.refreshToken);
       navigate("/");
     }
   } catch (err: any) {
