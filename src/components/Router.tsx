dfcb2844b24bd295f251fb371aec7e90df493bb1
Cartridge0923 <pjmin0923@gmail.com>
2025-06-06 21:03:58 +0900

    chore : fix Router #6

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Sidebar from "./common/Sidebar";
+
 const Router = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<Sidebar/>}
-      />
+      <Route path="/" />
       {/* <Route path="/login" element={<Login />} /> */}
       {/* <Route path="/login/verify" element={<Verify />} /> */}
     </Routes>
