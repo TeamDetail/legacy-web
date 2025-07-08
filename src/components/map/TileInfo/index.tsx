edfaf2da8c498b417c6ad30b5011885f081f1f3e
dksgusdn61 <dksgusdn61@gmail.com>
2025-07-08 17:48:04 +0900

    chore: 타일 정보 컴포넌트 수정

--- src/components/map/TileInfo/index.tsx
+++ src/components/map/TileInfo/index.tsx
@@ -1,42 +1,23 @@
 import LegacyButton from "@components/common/LegacyButton";
 import * as S from "./style";
-import { LegacyPalette, LegacySementic } from "@src/constants/color/color";
+import { LegacySementic } from "@src/constants/color/color";
 import useMapPixel from "@src/hooks/map/useMapPixel";
 
 interface TileInfoTypeProps {
   type: "legacy" | "default";
-  location: string;
-  requireCredit: string;
-  exploreStack: string;
 }
 
-const TileInfo = ({
-  type,
-  location,
-  requireCredit,
-  exploreStack,
-}: TileInfoTypeProps) => {
+const TileInfo = () => {
   const { ruinDetail } = useMapPixel();
 
   return (
     <S.InfoContainer>
       <S.HeadlineBold>블록 탐험</S.HeadlineBold>
       <S.Column8>
-        <S.LabelMedieum>선택한 블록</S.LabelMedieum>
-        <S.Row4>
-          <S.LocationText>{location}</S.LocationText>
-          <S.BlockTypeText $blockType={type}>
-            {type === "default" ? "일반" : "유적지"}
-          </S.BlockTypeText>
-        </S.Row4>
-      </S.Column8>
-      <S.Column8>
         <S.LabelMedieum>탐험 필요 크레딧</S.LabelMedieum>
         <S.Row4>
-          <S.CreditText>
-            {requireCredit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          </S.CreditText>
-          <S.CreditAddText>( 오늘 누적 {exploreStack}블록 )</S.CreditAddText>
+          <S.CreditText></S.CreditText>
+          <S.CreditAddText>( 오늘 누적 블록 )</S.CreditAddText>
         </S.Row4>
       </S.Column8>
       <LegacyButton
--- src/pages/Adventure/index.tsx
+++ src/pages/Adventure/index.tsx
@@ -2,8 +2,11 @@ import * as S from "./style";
 import GoogleMap from "@components/map/GoogleMap";
 import Sidebar from "@components/common/Sidebar";
 import TileInfo from "@components/map/TileInfo";
+import useMapPixel from "@src/hooks/map/useMapPixel";
 
 const Adventure = () => {
+  const { ruinDetail } = useMapPixel();
+
   return (
     <S.BackStage>
       <S.GoogleMapWrapper>
@@ -14,14 +17,11 @@ const Adventure = () => {
         <Sidebar />
       </S.Container>
 
-      <S.InfoPopup>
-        <TileInfo
-          type="legacy"
-          location="서울"
-          requireCredit="1000"
-          exploreStack="5"
-        />
-      </S.InfoPopup>
+      {ruinDetail && (
+        <S.InfoPopup>
+          <TileInfo/>
+        </S.InfoPopup>
+      )}
     </S.BackStage>
   );
 };
