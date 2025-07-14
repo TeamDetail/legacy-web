c1d4735b54762c2b04dfb52bf656a5f0c1f55918
Cartridge0923 <pjmin0923@gmail.com>
2025-07-14 15:58:23 +0900

    chore: fix getCardByRegion api params #13

--- src/api/card/card.api.ts
+++ src/api/card/card.api.ts
@@ -1,10 +1,10 @@
 import customAxios from "@src/libs/axios/customAxios";
-import { CardResponse } from "@src/types/card/card.type";
+import { CardResponse, RegionAttributeType } from "@src/types/card/card.type";
 import { BaseResponse } from "@src/types/globalType/global.type";
 
 class CardApi {
-  public async getCardByRegion(region: string): Promise<BaseResponse<CardResponse[]>> {
-    const { data } = await customAxios.get(`/card/collect/${region}`);
+  public async getCardByRegion(region: RegionAttributeType): Promise<BaseResponse<CardResponse[]>> {
+    const { data } = await customAxios.get(`/card/collection/${region}`);
     return data;
   }
 }
