dc7116b4a0de9a4604505e5db95380156cfdb281
Cartridge0923 <pjmin0923@gmail.com>
2025-07-17 21:19:50 +0900

    Update index.tsx

--- src/components/profile/UserRecord/index.tsx
+++ src/components/profile/UserRecord/index.tsx
@@ -12,19 +12,19 @@ const UserRecord = () => {
       <S.ProgressBars>
         <ProgressBar
           title="탐험"
-          value={myUserData!.data.record.cardCount}
+          value={myUserData!.data.record!.cardCount}
           max={450}
           barColor={LegacySementic.blue.netural}
           bgColor={LegacyPalette.fillNormal}
-          label={`카드 ${myUserData?.data.record.cardCount}장 수집`}
+          label={`카드 ${myUserData?.data.record?.cardCount}장 수집`}
         />
         <ProgressBar
           title="시련"
-          value={myUserData!.data.record.maxFloor}
+          value={myUserData!.data.record!.maxFloor}
           max={200}
           barColor={LegacyPalette.primaryNormal}
           bgColor={LegacyPalette.fillNormal}
-          label={`최고 ${myUserData?.data.record.maxFloor}층`}
+          label={`최고 ${myUserData?.data.record?.maxFloor}층`}
         />
         <ProgressBar
           title="숙련"
@@ -37,10 +37,10 @@ const UserRecord = () => {
       </S.ProgressBars>
       <S.Records>
         기록
-        <RecordItem title="탐험한 전체 블록 수" value={myUserData!.data.record.allBlocks} unit="블록" />
-        <RecordItem title="탐험한 유적지 블록 수" value={myUserData!.data.record.ruinsBlocks} unit="블록" />
-        <RecordItem title="수집한 모든 카드 수" value={myUserData!.data.record.cardCount} unit="장" />
-        <RecordItem title="수집한 찬란한 카드 수" value={myUserData!.data.record.shiningCardCount} unit="장" />
+        <RecordItem title="탐험한 전체 블록 수" value={myUserData!.data.record!.allBlocks} unit="블록" />
+        <RecordItem title="탐험한 유적지 블록 수" value={myUserData!.data.record!.ruinsBlocks} unit="블록" />
+        <RecordItem title="수집한 모든 카드 수" value={myUserData!.data.record!.cardCount} unit="장" />
+        <RecordItem title="수집한 찬란한 카드 수" value={myUserData!.data.record!.shiningCardCount} unit="장" />
         {/* <RecordItem title="시련 최고 층수" value={userData.maxFloor} unit="층" />
         <RecordItem title="시련 최고 점수" value={userData.maxScore} unit="문명 점수" /> */}
       </S.Records>
