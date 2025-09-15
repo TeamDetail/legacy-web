a8d0c2f9e5c76b224a4a6dba8e2db25e29bab8d5
dksgusdn61 <dksgusdn61@gmail.com>
2025-09-16 00:34:10 +0900

    feat: Description on Detail and Description Modal

--- src/components/map/TileInfo/Detail/index.tsx
+++ src/components/map/TileInfo/Detail/index.tsx
@@ -1,39 +1,51 @@
 import Card from "@components/common/Card";
 import * as S from "./style";
 import { RuinDetail } from "@src/types/map/ruin.type";
+import { LegacyModal } from "@components/common/LegacyModal";
+import { useState } from "react";
+import DescriptionModal from "./DescriptionModal";
 
 const Detail = ({ ruinDetail }: { ruinDetail: RuinDetail }) => {
+  const [isExplainModalOpen, setIsExplainModalOpen] = useState(false);
+
   return (
-    <S.DetailLayout>
-      <Card
-        cardType={ruinDetail.card.cardType}
-        cardId={ruinDetail.card.cardId}
-        cardImageUrl={ruinDetail.card.cardImageUrl}
-        cardName={ruinDetail.card.cardName}
-        size="L"
-        isAtv={false}
-        canInteract={false}
-        handleCardChange={() => {}}
-      />
-      <S.ExplainContainer>
-        <span>설명</span>
-        국군은 국가의 안전보장과 국토방위의 신성한 의무를 수행함을 사명으로
-        하며, 그 정치적 중립성은 준수된다.
-      </S.ExplainContainer>
-      <S.ExplorerContainer>
-        <S.DetailItem>
-          <span>탐험자 수</span>
-          23명
-        </S.DetailItem>
-        <S.DetailItem>
-          <span>획득 비율</span>
-          <S.RatioText>
-            <span>전체 중 </span>
-            3%
-          </S.RatioText>
-        </S.DetailItem>
-      </S.ExplorerContainer>
-    </S.DetailLayout>
+    <>
+      <S.DetailLayout>
+        <Card
+          cardType={ruinDetail.card.cardType}
+          cardId={ruinDetail.card.cardId}
+          cardImageUrl={ruinDetail.card.cardImageUrl}
+          cardName={ruinDetail.card.cardName}
+          size="L"
+          isAtv={false}
+          canInteract={false}
+          handleCardChange={() => {}}
+        />
+        <S.ExplainContainer onClick={() => setIsExplainModalOpen(true)}>
+          <span>설명</span>
+          {ruinDetail.description}
+        </S.ExplainContainer>
+        <S.ExplorerContainer>
+          <S.DetailItem>
+            <span>탐험자 수</span>
+            23명
+          </S.DetailItem>
+          <S.DetailItem>
+            <span>획득 비율</span>
+            <S.RatioText>
+              <span>전체 중 </span>
+              3%
+            </S.RatioText>
+          </S.DetailItem>
+        </S.ExplorerContainer>
+      </S.DetailLayout>
+      <LegacyModal isOpen={isExplainModalOpen} $background>
+        <DescriptionModal
+          close={() => setIsExplainModalOpen(false)}
+          description={ruinDetail.description}
+        />
+      </LegacyModal>
+    </>
   );
 };
 
