fe8acdbc38b565e76537cf7dd4ee1efe0591ba7e
dksgusdn61 <dksgusdn61@gmail.com>
2025-09-17 00:57:19 +0900

    feat: Shop Item Skeleton

--- src/components/shop/cardPack/index.tsx
+++ src/components/shop/cardPack/index.tsx
@@ -2,23 +2,31 @@ import PurchaseStats from "../purchaseStats";
 import styled from "styled-components";
 import CardpackItem from "./cardPackItem";
 import useShop from "@src/hooks/shop/useShop";
+import ShopItemSkeleton from "@components/skeleton/ShopItemSkeleton";
 
 const CardPack = () => {
   const { cardpacks, purchaseCardpackById } = useShop();
 
   return (
     <>
-      <CardPackContainer>
-        {cardpacks &&
-          cardpacks!.cardpack?.map((item) => (
-            <CardpackItem
-              key={item.cardpackId}
-              cardpackCost={item.price}
-              cardpackName={item.cardpackName}
-              onPurchase={() => purchaseCardpackById(item.cardpackId)}
-            />
-          ))}
-      </CardPackContainer>
+      {cardpacks ? (
+        <CardPackWrapper>
+          <CardPackContainer>
+            {cardpacks &&
+              cardpacks!.cardpack?.map((item) => (
+                <CardpackItem
+                  key={item.cardpackId}
+                  cardpackCost={item.price}
+                  cardpackName={item.cardpackName}
+                  onPurchase={() => purchaseCardpackById(item.cardpackId)}
+                />
+              ))}
+          </CardPackContainer>
+        </CardPackWrapper>
+      ) : (
+        <ShopItemSkeleton />
+      )}
+
       <PurchaseStats />
     </>
   );
@@ -29,12 +37,16 @@ export default CardPack;
 export const CardPackContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: fit-content(100%);
+  height: fit-content;
   grid-gap: 16px;
-  flex-grow: 1;
   @media (max-width: 1253px) {
     grid-template-columns: 1fr 1fr 1fr;
   }
   overflow-y: auto;
   align-items: start;
+`;
+
+export const CardPackWrapper = styled.div`
+  height: 100%;
+  overflow-y: auto;
 `;
