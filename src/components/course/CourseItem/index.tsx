1f4700bae06caf5ff07cc4a787545172e3266fde
dksgusdn61 <dksgusdn61@gmail.com>
2025-09-17 20:32:59 +0900

    feat: add cursor pointer on course item

--- src/components/course/CourseItem/index.tsx
+++ src/components/course/CourseItem/index.tsx
@@ -43,7 +43,6 @@ const CourseItem = ({
   disabled,
 }: CourseItemProps) => {
   const [isHearted, setIsHearted] = useState<boolean>(isHeart);
-  const [isCleared, setIsCleared] = useState<boolean>(isClear);
 
   const navigate = useNavigate();
 
@@ -87,9 +86,8 @@ const CourseItem = ({
           <CourseToggleButton
             buttonType="clear"
             value={clearCount}
-            isSelected={isCleared}
-            setIsSelected={setIsCleared}
-            disabled={disabled}
+            isSelected={isClear}
+            disabled={false}
           />
         </ToggleButtonContainer>
         <Tag data={tags} disabled />
@@ -104,6 +102,7 @@ const CourseItemContainer = styled.div<{ $size: "small" | "big" }>`
   display: flex;
   flex-direction: column;
   gap: ${({ $size }) => ($size === "small" ? "8px" : "20px")};
+  cursor: ${({ $size }) => $size === "small" && "pointer"};
 `;
 
 const CourseThumbnail = styled.div<{ $url: string; $size: "small" | "big" }>`
