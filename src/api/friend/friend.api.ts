6a4923a7163008ad8df666d2f209c27bd3dec9a1
dksgusdn61 <dksgusdn61@gmail.com>
2025-10-06 00:36:24 +0900

    fix: response type

--- src/api/friend/friend.api.ts
+++ src/api/friend/friend.api.ts
@@ -4,19 +4,21 @@ import { Friend, FriendRequest } from "@src/types/friend/friend.type";
 class FriendApi {
   public async getMyFriends(): Promise<Friend[]> {
     const { data } = await customAxios.get("/friends");
-    return data;
+    return data.data;
   }
 
   public async getMyFriendRequests(
-    type: "sents" | "requests"
+    type: "sent" | "requests"
   ): Promise<FriendRequest[]> {
     const { data } = await customAxios.get(`/friends/${type}`);
-    return data;
+    return data.data;
   }
 
   public async getMyFriendCode(): Promise<string> {
-    const { data } = await customAxios.get("/friends/code");
-    return data;
+    const { data } = await customAxios.get("/friends/my-code");
+    return data.data;
+  }
+
   }
 }
 
