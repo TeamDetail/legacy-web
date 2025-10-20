8e10d5ebf43129ce502cd6721c8944ebc5ad27a8
dksgusdn61 <dksgusdn61@gmail.com>
2025-10-20 21:26:11 +0900

    fix: import svg and put svg in index.html

--- src/components/achievement/GlobalAchievementSvgSprite/index.tsx
+++ src/components/achievement/GlobalAchievementSvgSprite/index.tsx
@@ -1,6 +1,18 @@
-import { createPortal } from "react-dom"
-import AchievementSvgSprite from "@src/assets/achievementBadge/achievementBadgeSvg.svg"
+import { createPortal } from "react-dom";
+import AchievementSvgSprite from "@src/assets/achievementBadge/achievementBadgeSvg.svg?raw";
+import styled from "styled-components";
 
-const GlobalAchievementSvgSprite = () => createPortal(AchievementSvgSprite, document.body)
+const GlobalAchievementSvgSprite = () =>
+  createPortal(
+    <GlobalAchievementSvgSpriteContainer
+      className="hidden"
+      dangerouslySetInnerHTML={{ __html: AchievementSvgSprite }}
+    />,
+    document.body
+  );
 
-export default GlobalAchievementSvgSprite
+export default GlobalAchievementSvgSprite;
+
+const GlobalAchievementSvgSpriteContainer = styled.div`
+  display: none;
+`
