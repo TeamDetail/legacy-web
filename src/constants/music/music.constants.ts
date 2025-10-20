a42d3143eb9bd9fc96369ac9ea7de34d59a1e674
Cartridge0923 <pjmin0923@gmail.com>
2025-10-21 00:12:09 +0900

    feat: add music #45

--- src/constants/music/music.constants.ts
+++ src/constants/music/music.constants.ts
@@ -2,10 +2,14 @@
 export const MUSICS = [
   {
     value: ["default"],
-    url: "src/assets/music/profile.mp3"
+    url: "src/assets/music/mainbgm.mp3",
   },
   {
-    value: ["ranking"],
-    url: "src/assets/music/oh.mp3",
+    value: ["login"],
+    url: "src/assets/music/loginbgm.mp3",
+  },
+  {
+    value: ["shop"],
+    url: "src/assets/music/marketbgm.mp3",
   },
 ];
