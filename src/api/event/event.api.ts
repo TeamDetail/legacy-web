74b44a788356d1175e2a85fb478c1dbf7fc81416
dksgusdn61 <dksgusdn61@gmail.com>
2025-12-03 09:22:13 +0900

    fix: event endpoint

--- src/api/event/event.api.ts
+++ src/api/event/event.api.ts
@@ -3,12 +3,12 @@ import { Event, EventDetail } from "@src/types/event/event.type";
 
 class EventApi {
   public async getEvent(): Promise<Event[]> {
-    const { data } = await customAxios.get("/event");
+    const { data } = await customAxios.get("/events");
     return data.data;
   }
 
   public async getEventDetail(id: number): Promise<EventDetail> {
-    const { data } = await customAxios.get(`/event/${id}`);
+    const { data } = await customAxios.get(`/events/${id}`);
     return data.data;
   }
 }
